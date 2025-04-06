eb246b56b0fb289200e812684db7de59ad0edb3b
Muhannad-09 <muhannadgsgs@gmail.com>
2025-04-06 14:21:42 +0000

    0.New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,24 @@
-test
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - inserts a node into a binary tree
+ * @parent: the parent of the node
+ * @value: the value of the node
+ * Return: a ptr to the node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(*newnode));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+	return (newnode);
+}
