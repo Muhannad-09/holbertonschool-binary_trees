6644c904b8417a34f9f3748b5395ae5c0cbd6aff
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-04-08 13:45:49 +0000

    38.Heap - Insert

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,16 +2,14 @@
 
 /**
  * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure the size
- * Return: size of the binary tree
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: size of the tree or 0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
-
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	size = binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
-	return (size);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
